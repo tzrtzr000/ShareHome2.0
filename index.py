f7ecba942bda7845defde6f42021510b347d24bf
Yi Shen <yblurobe@gmail.com>
2017-11-29 18:09:10 -0600

    Support task add now

--- index.py
+++ index.py
@@ -10,7 +10,6 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-
 # Create a sql database connection
 def database_connect():
     host_name = rds_config.host_name
@@ -18,21 +17,23 @@ def database_connect():
     db_password = rds_config.db_password
     db_name = rds_config.db_name
     try:
-        global cnx = pymysql.connect(host=host_name, user=db_username, password=db_password,
+        global cnx
+        global cursor
+        cnx = pymysql.connect(host=host_name, user=db_username, password=db_password,
                               db=db_name)
-        global cursor = cnx.cursor()
+        cursor = cnx.cursor()
         print("db connection established")
         return cursor
-
     except:
         return generate_error_response(500,
                                        "Database connection failed, please try again"
                                        "Database connection failed")
 def establish_boto3_client():
-    global boto_client = boto3.client(
-    'cognito-idp',
-    aws_access_key_id=aws_config.aws_access_key_id,
-    aws_secret_access_key=aws_config.aws_secret_access_key
+    global boto_client
+    boto_client = boto3.client(
+        'cognito-idp',
+        aws_access_key_id=aws_config.aws_access_key_id,
+        aws_secret_access_key=aws_config.aws_secret_access_key
     )
     return
 
@@ -47,17 +48,21 @@ def generate_error_response(error_code, body):
 def group_handler(event, context):
     body = event['body']
     print('In Group handler, request body is ' + body)
-    if 'operation' not in body:
+    json_body = json.loads(event['body'])
+
+    if 'operation' not in json_body:
         return generate_error_response(400, 'Missing \'operation\' key in request body')
     
     establish_boto3_client()
     # Now we have the client
-    operation = body['operation']
-    if (operation == 'createGroup'):
-        
-    else if (operation == 'addUser'):
-        
-    else if (operation == 'removeUser')
+    operation = json_body['operation']
+
+    if operation == 'createGroup':
+        pass
+    elif operation == 'addUser':
+        pass
+    elif operation == 'removeUser':
+        pass
     response = boto_client.list_users(
         UserPoolId=aws_config.UserPoolId
     )
@@ -69,6 +74,46 @@ def group_handler(event, context):
     return generate_success_response(json.loads(response))
 
 
+def task_handler(event, context):
+    body = event['body']
+    print('In task handler, request body is ' + body)
+    json_body = json.loads(event['body'])
+
+    if 'operation' not in json_body:
+        return generate_error_response(400, 'Missing \'operation\' key in request body')
+
+    # establish_boto3_client()
+    # Now we have the client
+    operation = json_body['operation']
+
+    database_connect()
+
+    table_name = 'Tasks'
+    group_name = json_body['groupName']
+
+    sql = 'SELECT * FROM %s WHERE groupName =\'%s\'' % (table_name, group_name)
+    print(sql)
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+
+    data = {
+        'result': json.dumps(rows),
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+
+    if operation == 'addPost':
+        pass
+    elif operation == 'removePost':
+        pass
+    elif operation == 'removeUser':
+        pass
+
+    # if body['operation'] == 'create':
+    #    # create a new group
+    #    cursor = database_connect()
+    return generate_success_response(data)
+
+
 def handler(event, context):
     print(json.dumps(event, sort_keys=True))
     print(context)
@@ -77,6 +122,8 @@ def handler(event, context):
 
     if resource_path == '/group':
         return group_handler(event, context)
+    elif resource_path == '/task':
+        return task_handler(event, context)
 
     # cursor = database_connect()
 
