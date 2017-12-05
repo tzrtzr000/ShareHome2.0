aa20c959db8bab4bdc831acde72b12deca01191d
Yi Shen <yblurobe@gmail.com>
2017-12-05 13:04:51 -0600

    Refactor server side code

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ logger.setLevel(logging.INFO)
 
 
 # Create a sql database connection
-def database_connect():
+def init_db_connection():
     host_name = rds_config.host_name
     db_username = rds_config.db_username
     db_password = rds_config.db_password
@@ -30,7 +30,9 @@ def database_connect():
         return generate_error_response(500,
                                        "Database connection failed, please try again"
                                        "Database connection failed")
-def establish_boto3_client():
+
+
+def init_boto3_client():
     global boto_client
     boto_client = boto3.client(
         'cognito-idp',
@@ -55,7 +57,7 @@ def group_handler(event, context):
     if 'operation' not in json_body:
         return generate_error_response(400, 'Missing \'operation\' key in request body')
     
-    establish_boto3_client()
+    init_boto3_client()
     # Now we have the client
     operation = json_body['operation']
 
@@ -77,34 +79,23 @@ def group_handler(event, context):
 
 
 def task_handler(event, context):
-    queryStringParameters = event["queryStringParameters"]
-    #print('In task handler, query parameter is ' + queryStringParameters)
-
-    if 'operation' not in queryStringParameters:
-        return generate_error_response(400, 'Missing \'operation\' key in request body')
-
-    # establish_boto3_client()
-    # Now we have the client
-    operation = queryStringParameters['operation']
-
-    database_connect()
+    init_db_connection()
 
     table_name = 'Tasks'
-    group_name = queryStringParameters['groupName']
 
-    # sql = 'SELECT * FROM %s WHERE groupName =\'%s\'' % (table_name, group_name)
-    # print(sql)
-    # cursor.execute(sql)
-    # rows = cursor.fetchall()
-    #
-    # data = {
-    #     'result': json.dumps(rows),
-    #     'timestamp': datetime.datetime.utcnow().isoformat()
-    # }
+    query_string_parameters = event["queryStringParameters"]
+    if query_string_parameters is None:
+        return generate_error_response(400, 'Missing query_string_parameters')
+
+    operation = query_string_parameters['operation']
+    group_name = query_string_parameters['groupName']
+
     if event['httpMethod'] == "GET":
+        if 'groupName' not in query_string_parameters:
+            return generate_error_response(400, 'Missing \'groupName\' key in request body')
+
         sql = 'SELECT groupName, taskTitle, taskContent, taskDuration, taskUser, taskSolved FROM %s WHERE groupName =\'%s\'' % (
-        'Tasks', 'lambdaTestGroup')
-        print(sql)
+            table_name, group_name)
         cursor.execute(sql)
         rows = cursor.fetchall()
         row_array_list = []
@@ -119,26 +110,33 @@ def task_handler(event, context):
             row_array_list.append(d)
 
         data = json.dumps(row_array_list)
+    elif event['httpMethod'] == "POST":
+        if 'operation' not in query_string_parameters:
+            return generate_error_response(400, 'Missing \'operation\' key in request body')
 
+        task = json.loads(event['body'])
 
-    elif operation == 'addTask':
-        sql = 'INSERT INTO Tasks (groupName, taskTitle, taskContent, taskDuration, taskUsers, taskSolved)' % (table_name, group_name)
-        print(sql)
-        cursor.execute(sql)
-        rows = cursor.fetchall()
+        if operation == 'addTask':
+            sql = 'INSERT INTO %s (groupName, taskTitle, taskContent, taskDuration, taskUsers, taskSolved) ' \
+                  'VALUES (%s,%s,%s,%s,%s,%s)' % (
+                table_name, task['groupName'], task['taskTitle'], task['taskContent'], task['taskDuration'],
+                task['taskUsers'], task['taskSolved'] )
 
-        data = {
-            'result': json.dumps(rows),
-            'timestamp': datetime.datetime.utcnow().isoformat()
-        }
-    elif operation == 'removeTask':
-        pass
-    elif operation == 'removeTask':
-        pass
+            print(sql)
+            cursor.execute(sql)
+            rows = cursor.fetchall()
 
-    # if body['operation'] == 'create':
-    #    # create a new group
-    #    cursor = database_connect()
+            data = {
+                'result': json.dumps(rows),
+                'timestamp': datetime.datetime.utcnow().isoformat()
+            }
+
+        if operation == 'removeTask':
+            data = {
+                'unknown': 'unknown'
+            }
+            pass
+
     return generate_success_response(data)
 
 
@@ -152,22 +150,6 @@ def handler(event, context):
         return group_handler(event, context)
     elif resource_path == '/task':
         return task_handler(event, context)
-
-    # cursor = database_connect()
-
-    # table_name = 'Groups'
-    # sql = "SELECT * FROM %s" % (table_name)
-    # cursor.execute(sql)
-    # if cursor.rowcount == 0:
-    #     return generate_error_response(201, "Please download and run the client software on your computer to complete"
-    #                                         " account linking and pairing."
-    #                                         " You can find the download link in the skill's description part.")
-    #
-    # cursor_result = [item['id'] for item in cursor.fetchall()]
-    # data = {
-    #     'output': json.dumps(event),
-    #     'timestamp': datetime.datetime.utcnow().isoformat()
-    # }
 
     return generate_error_response(404, 'Unsupported path')
 
