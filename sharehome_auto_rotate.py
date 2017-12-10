e873d4b9f3e36d3fc0be9d755f255709eaae8e54
Yi Shen <yblurobe@gmail.com>
2017-12-10 00:19:38 -0600

    Now notification should be working

--- sharehome_auto_rotate.py
+++ sharehome_auto_rotate.py
@@ -3,6 +3,8 @@ from __future__ import print_function
 import os
 import library
 import index
+import boto3
+import aws_config
 from datetime import datetime, timedelta
 from collections import deque
 
@@ -10,6 +12,13 @@ from collections import deque
 def lambda_handler(event, context):
     library.init()
     index.init_db_connection()
+
+    global boto_cognito_client
+    boto_cognito_client = boto3.client(
+        'cognito-idp',
+        aws_access_key_id=aws_config.aws_access_key_id,
+        aws_secret_access_key=aws_config.aws_secret_access_key
+    )
 
     table_name = 'Tasks'
 
@@ -39,7 +48,17 @@ def lambda_handler(event, context):
 
             # send notification to user
             user_name = new_order[1]
-
+            try:
+                boto_cognito_client.admin_get_user(
+                    UserPoolId=aws_config.UserPoolId,
+                    Username=user_name
+                    )
+            except boto_cognito_client.exceptions.UserNotFoundException as e:
+                pass
+                print("Invalid user: %s" % user_name)
+            else:
+                index.push_notification(user_name, None, row[4], "It's your turn now")
+                print("%s has been notified for %s" % (user_name, row[4]))
     index.close_db_connection()
 
 
